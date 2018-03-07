ace753f27f2b996087a486440f629d2d5ee3a8d6
swati biswas <swati_biswas@persistent.co.in>
2018-03-07 10:26:33 +0000

    Second commit

--- cookbooks/test_cookbook/recipes/test_user.rb
+++ cookbooks/test_cookbook/recipes/test_user.rb
@@ -3,3 +3,11 @@
 # Recipe:: test_user
 #
 # Copyright:: 2018, Swati Biswas, All Rights Reserved.
+#
+package 'net-tools' do
+ action :install
+end
+
+package 'httpd' do
+ action :install
+end
--- cookbooks/test_cookbook/spec/unit/recipes/default_spec.rb
+++ cookbooks/test_cookbook/spec/unit/recipes/default_spec.rb
@@ -7,29 +7,20 @@
 require 'spec_helper'
 
 describe 'test_cookbook::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  context 'CentOS' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs httpd' do
+      expect(chef_run).to install_package('httpd')
     end
   end
 end
